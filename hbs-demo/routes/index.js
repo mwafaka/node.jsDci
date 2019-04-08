97f6217861377de973d5d1fc0ba808b4a4832a31
mwafak <Mwafaqarbash3@gmail.com>
2019-04-08 11:53:37 +0200

    first commit

--- hbs-demo/routes/index.js
+++ hbs-demo/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Express",
+    condition: true,
+    numbers: [1, 2, 3, 4],
+    list: [
+      { fruits: ["Mango", "Apple", "Grapes"] },
+      { veges: ["Potato", "Avacado", "Onion"] }
+    ]
+  });
 });
 
 module.exports = router;
